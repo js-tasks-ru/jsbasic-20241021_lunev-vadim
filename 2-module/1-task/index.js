93be90a2e98792a9de0d9dfdebff15d6c362363e
vadim <lunev82@gmail.com>
2024-11-15 09:52:42 +0300

    1-3 tasks of 2 module

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,9 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+  for (let key in salaries) {
+    if (isFinite(salaries[key]) && !isNaN(salaries[key]) && typeof salaries[key] === "number") {
+      sum += salaries[key];
+    }
+  }
+  return sum;
 }
