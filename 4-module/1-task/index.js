142c2b794fe449661b20ab22f4c6aeec71f9b1a5
vadim <lunev82@gmail.com>
2024-11-18 00:55:27 +0300

    1-3 tasks of 4 module

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,7 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let ul = document.createElement('ul');
+  friends.map((item) => {
+    ul.innerHTML += `<li>${item.firstName} ${item.lastName}</li>`;
+  });
+  return ul;
 }
--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,8 @@
 function makeDiagonalRed(table) {
-  // ваш код...
+  let tableElement = table.firstElementChild;
+  for (let i = 0; i < tableElement.children.length; i++) {
+    let row = tableElement.children[i];
+    row.children[i].style.backgroundColor = "red";
+  }
 }
+
